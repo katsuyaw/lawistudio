d030b292a9aefb1b2cc753196c0c90d0a474cd4b
katsuyaw <kw@kmail.me>
2021-07-04 11:02:35 -0700

    Remove tos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,8 +8,8 @@ const Footer = (props) => {
       <div className="relative">
         <div className="absolute bottom-1 left-0">
           <div className="flex flex-row gap-2 text-gray-500 text-xs">
-            <Link to="/privacy">privacy</Link>
-            <Link to="/terms">terms</Link>
+            {/* <Link to="/privacy">privacy</Link>
+            <Link to="/terms">terms</Link> */}
           </div>
         </div>
       </div>
