8e4691e6d80ae3ca5ecf6dca7ebd2e45df8a07d0
katsuyaw <kw@kmail.me>
2021-07-04 13:34:38 -0700

    Add team

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,23 +1,76 @@
 import React from "react";
 import Card from "../components/Card";
-import { Link } from "react-router-dom";
+import "../App.css";
 
 const team = () => {
   return (
     <>
       <Card
         title=""
-        body={[
-          <div className="flex flex-row gap-8 text-sm">
-            <p>GABE HILT </p>
-            <a
-              href="https://instagram.com/gabehilt"
-              className="text-gray-500 hover:text-white"
-            >
-              @gabehilt
-            </a>
-          </div>,
-        ]}
+        body={
+          <div className="flex flex-col text-xs gap-4">
+            <div className="team-holder">
+              <p className="team-position">Don</p>
+              <p className="team-name">GABE</p>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://instagram.com/gabehilt"
+                className="team-contact"
+              >
+                @gabehilt
+              </a>
+            </div>
+            <div className="team-holder">
+              <p className="team-position">Consigliere</p>
+              <p className="team-name">Danielle</p>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://instagram.com/bydaniellekim"
+                className="team-contact"
+              >
+                @bydaniellekim
+              </a>
+            </div>
+            <div className="team-holder">
+              <p className="team-position">Global Affairs</p>
+              <p className="team-name">Logan</p>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://instagram.com/logancaytonm"
+                className="team-contact"
+              >
+                @logancayton
+              </a>
+            </div>
+            <div className="team-holder">
+              <p className="team-position">Design</p>
+              <p className="team-name">Sierra</p>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://instagram.com/madebysi.co "
+                className="team-contact"
+              >
+                @madebysi.co
+              </a>
+            </div>
+            <div className="team-holder">
+              <p className="team-position">Architecture</p>
+              <p className="team-name">Katsuya</p>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://instagram.com/katsuyawashere "
+                className="team-contact"
+              >
+                @katsuyawashere
+              </a>
+            </div>
+          </div>
+        }
         moreNav="true"
       />
     </>
