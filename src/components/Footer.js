cba3d856d494bad97a2edce01d27493c5a01b40c
katsuyaw <kw@kmail.me>
2021-07-04 11:08:13 -0700

    Fix privacy and tos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,11 @@ const Footer = (props) => {
   const more = props.more;
   return (
     <>
-      <div className="relative">
-        <div className="absolute bottom-1 left-0">
-          <div className="flex flex-row gap-2 text-gray-500 text-xs">
-            {/* <Link to="/privacy">privacy</Link>
-            <Link to="/terms">terms</Link> */}
+      <div className="relative w-screen">
+        <div className="absolute bottom-1 right-5">
+          <div className="flex flex-row gap-2 text-gray-500 text-xs ">
+            <Link to="/privacy">privacy</Link>
+            <Link to="/terms">terms</Link>
           </div>
         </div>
       </div>
